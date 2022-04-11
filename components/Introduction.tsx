ec1a7e1b7c1c4949f497ed25fb50f96f6ab79f93
Nevil <nevilmills@gmail.com>
2022-04-11 13:11:20 -0700

    setup tailwind in project

---
+++ components/Introduction.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Introduction: React.FC<{}> = ({}) => {
+  return (
+    <div className="flex flex-col items-center w-full">
+      <div>Hello</div>
+    </div>
+  );
+};
