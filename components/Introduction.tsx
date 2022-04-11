e131c906f0ac25acabee302a5b6d69feb848e125
Nevil <nevilmills@gmail.com>
2022-04-11 15:00:22 -0700

    add basic intro paragraph

--- components/Introduction.tsx
+++ components/Introduction.tsx
@@ -2,8 +2,16 @@ import React from "react";
 
 export const Introduction: React.FC<{}> = ({}) => {
   return (
-    <div className="flex flex-col items-center w-full">
-      <div>Hello</div>
+    <div className="flex flex-col items-center w-full pt-24">
+      <div className="max-w-lg space-y-2">
+        <p className="text-2xl font-lato">Hi, my name is</p>
+        <p className="text-5xl font-lato">Nevil Mills.</p>
+        <p className="text-2xl font-lato">
+          I'm a software engineer from Chilliwack, B.C. Currently, I am focused
+          on developing full stack web applications as I search for a team of
+          developers to join.
+        </p>
+      </div>
     </div>
   );
 };
