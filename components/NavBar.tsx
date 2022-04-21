6e92b3830d8dec84c6b961ea5489aff34d8adcc4
Nevil <nevilmills@gmail.com>
2022-04-20 21:59:28 -0700

    Add: hide navbar on scroll

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { NavElement } from "./NavElement";
 
 export const NavBar: React.FC<{}> = ({}) => {
+  useEffect(() => {
+    document.addEventListener("wheel", (event) => {
+      if (event.deltaY > 0) {
+        document.getElementById("navbar")!.style.top = "-55px";
+      } else {
+        document.getElementById("navbar")!.style.top = "0px";
+      }
+    });
+  }, []);
+
   return (
-    <div className="nav">
+    <div id="navbar" className="nav">
       <div className="mr-8 space-x-6">
         <NavElement>About</NavElement>
         <NavElement>Experience</NavElement>
