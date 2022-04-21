ac3a743e5c70124ef8b318d8885d34bf12649c17
Nevil <nevilmills@gmail.com>
2022-04-21 13:32:03 -0700

    Update: bullet styles in education section

--- components/Education.tsx
+++ components/Education.tsx
@@ -7,20 +7,18 @@ export const Education: React.FC<{}> = ({}) => {
         University of the Fraser Valley
       </h4>
       <p className="mt-2">September 2015 - August 2021</p>
-      <ul className="list-disc space-y-2 mt-4">
+      <ul className="arrow-list list-disc space-y-2 mt-4">
         <li className="text-darkGreen">
           Bachelor of Science in Computer Science
         </li>
-        <li>
-          Studies included:
-          <ul className="list-disc ml-10 mt-2 space-y-2">
-            <li>Software Engineering</li>
-            <li>Advanced Website Programming</li>
-            <li>Extreme Computing</li>
-            <li>Object-oriented Modeling and Design</li>
-            <li>Introduction to Artificial Intelligence</li>
-          </ul>
-        </li>
+        <li>Studies included:</li>
+      </ul>
+      <ul className="list-disc ml-10 mt-2 space-y-2">
+        <li>Software Engineering</li>
+        <li>Advanced Website Programming</li>
+        <li>Extreme Computing</li>
+        <li>Object-oriented Modeling and Design</li>
+        <li>Introduction to Artificial Intelligence</li>
       </ul>
     </div>
   );
