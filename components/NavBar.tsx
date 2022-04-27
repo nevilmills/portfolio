54578d60cf07fa83ff57c6cce7c2e41469ebd93f
Nevil <nevilmills@gmail.com>
2022-04-27 16:47:45 -0700

    Add: social media icons & links to navbar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,5 +1,7 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useEffect } from "react";
 import { NavElement } from "./NavElement";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 export const NavBar: React.FC<{}> = ({}) => {
   useEffect(() => {
@@ -14,6 +16,17 @@ export const NavBar: React.FC<{}> = ({}) => {
 
   return (
     <div id="navbar" className="nav">
+      <div className="pl-4 space-x-4">
+        <a href="https://github.com/nevilmills" target="_blank">
+          <FontAwesomeIcon icon={faGithub} className="text-lightestGrey" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/nevil-mills-4655021a1/"
+          target="_blank"
+        >
+          <FontAwesomeIcon icon={faLinkedin} className="text-lightestGrey" />
+        </a>
+      </div>
       <div className="mr-8 space-x-6">
         <NavElement href="/#about">About</NavElement>
         <NavElement href="/#experience">Experience</NavElement>
