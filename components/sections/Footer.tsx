cb414edc0466f82215f5fe4a614224c6265112ed
Nevil <nevilmills@gmail.com>
2022-04-28 11:00:14 -0700

    Add: linkedin link to footer

--- components/sections/Footer.tsx
+++ components/sections/Footer.tsx
@@ -7,7 +7,11 @@ export const Footer: React.FC<FooterProps> = ({}) => {
     <footer className="p-4 w-full text-center">
       <div className="primaryText">
         Developed by{" "}
-        <a className="highlighted" href="">
+        <a
+          className="highlighted"
+          href="https://www.linkedin.com/in/nevil-mills-4655021a1/"
+          target="_blank"
+        >
           Nevil Mills
         </a>
       </div>
